c23137a2a78252984f52ad25f25033c4835c2622
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-06-27 13:21:23 -0300

    Allow editing description in the focus mode

--- src/routes/focus/index.svelte
+++ src/routes/focus/index.svelte
@@ -7,6 +7,7 @@
 	import { calculateNextDate } from '$lib/checkRecurrentActions';
 
 	let action;
+	let descriptionNode;
 	$: action = $actionsStore[0];
 
 	if (action) {
@@ -25,6 +26,10 @@
 			'recurrence.nextDate': calculateNextDate(action.recurrence)
 		});
 	};
+
+	$: {
+		console.log('descriptionNode', descriptionNode);
+	}
 </script>
 
 <svelte:head>
@@ -41,7 +46,19 @@
 	class='w-full h-full inline-flex flex-col gap-6 p-4 justify-center items-center'>
 	<p class='text-bg'>{action ? action?.title : "You're done!"}</p>
 	<p
-		class='font-light text-md'>{action ? action?.description : "There's nothing else to do."}</p>
+		bind:this={descriptionNode}
+		on:click={() => {
+			descriptionNode.contentEditable = true;
+			descriptionNode.focus();
+		}}
+		on:blur={() => {
+			descriptionNode.contentEditable = false;
+			actions.save({ ...action, description: descriptionNode.innerHTML });
+		}}
+		class='font-light text-md'
+	>
+		{@html action ? action?.description : "There's nothing else to do."}
+	</p>
 </div>
 
 {#if action}
