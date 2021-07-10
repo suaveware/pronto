d9196963b471dcf2f42d0987c7cc94d7167ccdba
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-07-10 15:50:23 -0300

    Remove fly animation that was making page change not responsive

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ yarn build
 touch build/.nojekyll
 # Contains the url of our site to serve in a custom domain
 echo "pronto.suaveware.dev" > build/CNAME
-git init build/
+git -C build/ init
 git -C build/ remote add origin git@github.com:luizcarlos1405/pronto.git
 git -C build/ add -A
 git -C build/ push origin master:deploy -f
