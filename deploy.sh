d502e2fdc9aae5a29de951ae3763dd3edc43e9f7
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-07-10 15:53:07 -0300

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@ echo "pronto.suaveware.dev" > build/CNAME
 git -C build/ init
 git -C build/ remote add origin git@github.com:luizcarlos1405/pronto.git
 git -C build/ add -A
+git -C build/ commit -m "deploy"
 git -C build/ push origin master:deploy -f
