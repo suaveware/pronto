3889b7371885e296bc86f6cf99276246e7f6566c
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-07-15 11:33:22 -0300

    Update prettier configuration and format some of the files

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,15 +6,10 @@
 	import ActivityForm from '$lib/ActivityForm.svelte';
 	import ActivityCard from '$lib/ActivityCard.svelte';
 	import { longpress } from '$lib/custom-actions/longpress';
-	import {
-		PlusIcon,
-		MaximizeIcon,
-		XIcon,
-		InfoIcon,
-		ChevronRightIcon,
-	} from 'svelte-feather-icons';
+	import { PlusIcon, MaximizeIcon, XIcon, InfoIcon, ChevronRightIcon } from 'svelte-feather-icons';
 	import { base } from '$app/paths';
 	import { ACTIVITIES_STATE } from '$lib/constants';
+	import { DateTime } from 'luxon';
 	import { List } from 'immutable';
 	import Separator from '$lib/components/Separator.svelte';
 	import { version } from '/package.json';
@@ -28,10 +23,19 @@
 	let editingActivity = null;
 	let activitiesByState;
 	let dndActivities; // Svelte dnd needs mutable stuff
+	let waitingActivities;
 
 	$: {
 		activitiesByState = $state.activities.groupBy(activity => activity.state);
 		dndActivities = activitiesByState.get(ACTIVITIES_STATE.READY.key, List()).toJS();
+		waitingActivities = activitiesByState
+			.get(ACTIVITIES_STATE.WAITING.key, List())
+			.sort(
+				(a, b) =>
+					DateTime.fromISO(a.recurrence.nextDate).toMillis() -
+					DateTime.fromISO(b.recurrence.nextDate).toMillis()
+			)
+			.toArray();
 	}
 
 	const handleMaximizePressed = () => {
@@ -69,86 +73,71 @@
 	};
 </script>
 
-
 <svelte:head>
 	<title>Pronto</title>
 </svelte:head>
 
-
-<div class='flex flex-col items-stretch bg-blueGray-600 h-full overflow-hidden'>
-	<div
-		class='flex justify-between items-center pb-8 px-4 pt-4 text-white rounded-b w-full'>
-		<div class='text-2xl'>Pronto</div>
-		<div
-			on:click={handleMenuClicked}
-			class='cursor-pointer'
-		>
+<div class="flex flex-col items-stretch bg-blueGray-600 h-full overflow-hidden">
+	<div class="flex justify-between items-center pb-8 px-4 pt-4 text-white rounded-b w-full">
+		<div class="text-2xl">Pronto</div>
+		<div on:click={handleMenuClicked} class="cursor-pointer">
 			{#if isSettingsOpen}
-				<XIcon size='24' />
+				<XIcon size="24" />
 			{:else}
-				<InfoIcon size='24' />
+				<InfoIcon size="24" />
 			{/if}
 		</div>
 	</div>
 
 	<div
-		class='transition-all ease-in-out duration-500 max-h-screen opacity-100'
+		class="transition-all ease-in-out duration-500 max-h-screen opacity-100"
 		class:hide={!isSettingsOpen}
 	>
 		{#if isSettingsOpen}
 			<div
-				class='text-white px-4 py-8 overflow-y-scroll inline-flex gap-2 text-xl flex-col items-stretch w-full'
-				style='height: 76vh'
+				class="text-white px-4 py-8 overflow-y-scroll inline-flex gap-2 text-xl flex-col items-stretch w-full"
+				style="height: 76vh"
 				transition:fade={{ duration: openSettingsDuration }}
 			>
 				{#if false}
 					This is how the options will look like once we have use for them
-					<button
-						class='inline-flex rounded px-2 w-full w-full items-center gap-3'>
-						<div>
-							<InfoIcon size='24' />
-						</div>
-						<div class=''>Sobre</div>
-						<div class='ml-auto'>
-							<ChevronRightIcon size='24' />
-						</div>
+					<button class="inline-flex rounded px-2 w-full w-full items-center gap-3">
+						<span>
+							<InfoIcon size="24" />
+						</span>
+						<span class="">Sobre</span>
+						<span class="ml-auto">
+							<ChevronRightIcon size="24" />
+						</span>
 					</button>
 				{/if}
 
-				<div class='inline-flex flex-col items-center pt-4 gap-2 w-full'>
-					<p>
-						by Suaveware
-					</p>
-					<p class='text-base text-justify'>
-						Este aplicativo é de código livre sob a licença GPL-3 e seu código
-						fonte
-						está disponível neste
-						<a
-							class='underline'
-							href='https://github.com/luizcarlos1405/pronto'
-						>
+				<div class="inline-flex flex-col items-center pt-4 gap-2 w-full">
+					<p>by Suaveware</p>
+					<p class="text-base text-justify">
+						Este aplicativo é de código livre sob a licença GPL-3 e seu código fonte está disponível
+						neste
+						<a class="underline" href="https://github.com/luizcarlos1405/pronto">
 							repositório no Github
 						</a>
 						.
 					</p>
-					<p class='text-base text-justify'>
-						Distribuído como <i>progressive web app</i> (PWA) através do
-						endereço:
+					<p class="text-base text-justify">
+						Distribuído como <i>progressive web app</i> (PWA) através do endereço:
 					</p>
-					<a class='underline text-base w-full'
-						 href='https://pronto.suaveware.dev'>
+					<a class="underline text-base w-full" href="https://pronto.suaveware.dev">
 						https://pronto.suaveware.dev
 					</a>
-					<Separator title='Contato' class='text-white mt-4' />
-					<div class='inline-flex text-base w-full flex-col gap-2'>
+					<Separator title="Contato" class="text-white mt-4" />
+					<div class="inline-flex text-base w-full flex-col gap-2">
 						<p>Email: luizcarlos1405@suaveware.dev</p>
 						<p>Twitter: @semmilho</p>
 						<p>YouTube: RSensato</p>
 						<p>TikTok: @RSensato</p>
 					</div>
 
-					<Separator title='Versão' class='text-white mt-4' />
-					<div class='text-sm'>
+					<Separator title="Versão" class="text-white mt-4" />
+					<div class="text-sm">
 						{version}
 					</div>
 				</div>
@@ -157,58 +146,48 @@
 	</div>
 
 	<div
-		class='p-4 pt-6 flex-grow transition-all duration-300 overflow-y-scroll flex-col bg-white shadow border-bluGray-400 inline-flex rounded-t-2xl gap-2'
+		class="p-4 pt-6 flex-grow transition-all duration-300 overflow-y-scroll flex-col bg-white shadow border-bluGray-400 inline-flex rounded-t-2xl gap-2"
 		class:overflowhidden={isSettingsOpen}
 		bind:this={scrollContainer}
 	>
 		{#if !$state.activities.size}
-			<span class='text-blueGray-800'>Nenhuma atividade, clique no botão de + para adicionar.</span>
+			<span class="text-blueGray-800">Nenhuma atividade, clique no botão de + para adicionar.</span>
 		{/if}
 
 		{#if dndActivities.length}
 			<div
-				class='w-full mb-10 flex-col inline-flex gap-2'
-				use:dndzone='{{
+				class="w-full mb-10 flex-col inline-flex gap-2"
+				use:dndzone={{
 					items: dndActivities,
 					dragDisabled: isSettingsOpen,
 					flipDurationMs,
-					customStartEvent: "longpress",
-				}}'
-				on:consider='{handleDnd}'
-				on:finalize='{handleDnd}'
+					customStartEvent: 'longpress',
+				}}
+				on:consider={handleDnd}
+				on:finalize={handleDnd}
 			>
 				{#each dndActivities as activity, index (activity._id)}
-			<span
-				animate:flip='{{ duration: flipDurationMs }}'
-				use:longpress
-			>
-				<ActivityCard
-					on:click={handleItemPressed(activity._id)}
-					activity={activity}
-				/>
-			</span>
+					<span animate:flip={{ duration: flipDurationMs }} use:longpress>
+						<ActivityCard on:click={handleItemPressed(activity._id)} {activity} />
+					</span>
 				{/each}
 			</div>
 		{/if}
 
 		{#if activitiesByState.get(ACTIVITIES_STATE.WAITING.key)?.size}
-			<Separator title={ACTIVITIES_STATE.WAITING.label} class='mt-4' />
+			<Separator title={ACTIVITIES_STATE.WAITING.label} class="mt-4" />
 		{/if}
-		{#each activitiesByState.get(ACTIVITIES_STATE.WAITING.key, List()).toArray() as activity, index (activity._id)}
-			<ActivityCard
-				on:click={handleItemPressed(activity._id)}
-				activity={activity}
-			/>
+		{#each waitingActivities as activity, index (activity._id)}
+			<ActivityCard on:click={handleItemPressed(activity._id)} {activity} />
 		{/each}
 
 		{#if activitiesByState.get(ACTIVITIES_STATE.DONE.key)?.size}
-			<Separator title={ACTIVITIES_STATE.DONE.label} class='mt-4' />
+			<Separator title={ACTIVITIES_STATE.DONE.label} class="mt-4" />
 		{/if}
-		{#each activitiesByState.get(ACTIVITIES_STATE.DONE.key, List()).toArray() as activity, index (activity._id)}
-			<ActivityCard
-				on:click={handleItemPressed(activity._id)}
-				activity={activity}
-			/>
+		{#each activitiesByState
+			.get(ACTIVITIES_STATE.DONE.key, List())
+			.toArray() as activity, index (activity._id)}
+			<ActivityCard on:click={handleItemPressed(activity._id)} {activity} />
 		{/each}
 	</div>
 </div>
@@ -216,42 +195,32 @@
 {#if editingActivity}
 	<ActivityForm bind:activity={editingActivity} />
 {:else if !isSettingsOpen}
-	<div
-		class='fixed bottom-4 text-blueGray-600 right-4 gap-3 items-center inline-flex flex-col'
-	>
-		<button
-			on:click={handlePlusPressed}
-			class='fab'
-			class:small={!!$state.activities.size}
-		>
+	<div class="fixed bottom-4 text-blueGray-600 right-4 gap-3 items-center inline-flex flex-col">
+		<button on:click={handlePlusPressed} class="fab" class:small={!!$state.activities.size}>
 			<PlusIcon size={$state.activities.size ? '20' : '28'} />
 		</button>
 		{#if $state.activities.size}
-			<button
-				on:click={handleMaximizePressed}
-				class='fab'
-			>
-				<MaximizeIcon size='28' />
+			<button on:click={handleMaximizePressed} class="fab">
+				<MaximizeIcon size="28" />
 			</button>
 		{/if}
 	</div>
 {/if}
 
 <style>
-    .fab {
-        @apply p-5 shadow bg-blueGray-500 text-white rounded-full;
-    }
+	.fab {
+		@apply p-5 shadow bg-blueGray-500 text-white rounded-full;
+	}
 
-    .small {
-        @apply p-2;
-    }
+	.small {
+		@apply p-2;
+	}
 
-    .hide {
-        @apply max-h-0 opacity-0;
-    }
+	.hide {
+		@apply max-h-0 opacity-0;
+	}
 
-    .overflowhidden {
-        @apply overflow-hidden;
-    }
-
+	.overflowhidden {
+		@apply overflow-hidden;
+	}
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,72 +7,73 @@ module.exports = {
 	// TODO: enable purging only for production somehow https://tailwindcss.com/docs/optimizing-for-production
 	purge: {
 		enabled: true,
-		content: ['./src/**/*.svelte']
+		content: ['./src/**/*.svelte'],
 	},
-	plugins: [require('@tailwindcss/forms'),
-		plugin(function({ addComponents, theme }) {
+	plugins: [
+		require('@tailwindcss/forms'),
+		plugin(function ({ addComponents, theme }) {
 			const screens = theme('screens', {});
 			addComponents([
 				{
-					'.container': { width: '100%' }
+					'.container': { width: '100%' },
 				},
 				{
 					[`@media (min-width: ${screens.sm})`]: {
 						'.container': {
-							'max-width': '640px'
-						}
-					}
+							'max-width': '640px',
+						},
+					},
 				},
 				{
 					[`@media (min-width: ${screens.md})`]: {
 						'.container': {
-							'max-width': '768px'
-						}
-					}
+							'max-width': '768px',
+						},
+					},
 				},
 				{
 					[`@media (min-width: ${screens.lg})`]: {
 						'.container': {
-							'max-width': '1024px'
-						}
-					}
+							'max-width': '1024px',
+						},
+					},
 				},
 				{
 					[`@media (min-width: ${screens.xl})`]: {
 						'.container': {
-							'max-width': '1280px'
-						}
-					}
+							'max-width': '1280px',
+						},
+					},
 				},
 				{
 					[`@media (min-width: ${screens['2xl']})`]: {
 						'.container': {
-							'max-width': '1280px'
-						}
-					}
-				}
+							'max-width': '1280px',
+						},
+					},
+				},
 			]);
-		})
+		}),
 	],
 	theme: {
 		colors,
 		fontFamily: {
-			'mono': ['Fira Mono'],
-			'sans': ['Fira Sans']
+			mono: ['Fira Mono'],
+			sans: ['Fira Sans'],
 		},
 		extend: {
 			minHeight: {
-				'screen-75': '75vh'
+				'screen-75': '75vh',
 			},
 			fontSize: {
-				'55': '55rem'
+				55: '55rem',
 			},
 			opacity: {
-				'80': '.8'
+				80: '.8',
 			},
 			zIndex: {
-				'2': 2,
-				'3': 3
+				2: 2,
+				3: 3,
 			},
 			inset: {
 				'-100': '-100%',
@@ -89,17 +90,17 @@ module.exports = {
 				'145-px': '145px',
 				'195-px': '195px',
 				'210-px': '210px',
-				'260-px': '260px'
+				'260-px': '260px',
 			},
 			height: {
 				'95-px': '95px',
 				'70-px': '70px',
 				'350-px': '350px',
 				'500-px': '500px',
-				'600-px': '600px'
+				'600-px': '600px',
 			},
 			maxHeight: {
-				'860-px': '860px'
+				'860-px': '860px',
 			},
 			maxWidth: {
 				'100-px': '100px',
@@ -108,16 +109,16 @@ module.exports = {
 				'180-px': '180px',
 				'200-px': '200px',
 				'210-px': '210px',
-				'580-px': '580px'
+				'580-px': '580px',
 			},
 			minWidth: {
 				'140-px': '140px',
-				'48': '12rem'
+				48: '12rem',
 			},
 			backgroundSize: {
-				full: '100%'
-			}
-		}
+				full: '100%',
+			},
+		},
 	},
 	variants: [
 		'responsive',
@@ -131,6 +132,6 @@ module.exports = {
 		'focus',
 		'active',
 		'visited',
-		'disabled'
-	]
+		'disabled',
+	],
 };
