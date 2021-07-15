1c72e94f59d18cd2cd425069a2feb9cf13766dfa
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-07-14 21:51:21 -0300

    Adds clear button to task quick input

--- src/lib/ActivityForm.svelte
+++ src/lib/ActivityForm.svelte
@@ -1,11 +1,7 @@
 <script>
 	import { RECURRENCE_TYPE } from '$lib/constants';
 	import { TrashIcon } from 'svelte-feather-icons';
-	import {
-		Activity, CheckItem,
-		removeActivity,
-		saveActivity,
-	} from '$lib/state';
+	import { Activity, CheckItem, removeActivity, saveActivity } from '$lib/state';
 	import { Info } from 'luxon';
 	import Input from './components/form/Input.svelte';
 	import Fieldset from './components/form/Fieldset.svelte';
@@ -34,12 +30,14 @@
 
 	// TODO: use the saveActivity here instead of weirdly sending it up
 	const handleConfirmPressed = () => {
-		saveActivity(Activity({
-			...activity.toJS(),
-			...form,
-			checkList: form.checkList.filter(({ name }) => name),
-			_id: activity._id,
-		}));
+		saveActivity(
+			Activity({
+				...activity.toJS(),
+				...form,
+				checkList: form.checkList.filter(({ name }) => name),
+				_id: activity._id,
+			})
+		);
 
 		activity = null;
 	};
@@ -51,13 +49,13 @@
 		form.checkList = [...form.checkList.filter(({ name }) => name), lastItem];
 	};
 
-	const handleTaskOnFocus = (item) => {
+	const handleTaskOnFocus = item => {
 		if (!item.name) {
 			form.checkList = [...form.checkList, CheckItem().toJS()];
 		}
 	};
 
-	const handleTaskKeyUp = (event) => {
+	const handleTaskKeyUp = event => {
 		if (event.key === 'Enter') {
 			const nextInputElement = event.target.parentElement?.nextElementSibling?.firstElementChild;
 
@@ -74,36 +72,21 @@
 </script>
 
 <div
-	class='fixed z-20 right-0 left-0 bottom-0 top-0 p-4 overflow-y-scroll inline-flex justify-start flex-col bg-blueGray-600 h-full'
+	class="fixed z-20 right-0 left-0 bottom-0 top-0 p-4 overflow-y-scroll inline-flex justify-start flex-col bg-blueGray-600 h-full"
 	bind:this={scrollContainer}
 >
-
-	<Fieldset
-		title='Atividade'
-		class='bg-blueGray-100 p-4 relative rounded shadow mb-4'
-	>
+	<Fieldset title="Atividade" class="bg-blueGray-100 p-4 relative rounded shadow mb-4">
 		{#if activity?._id}
-			<button
-				on:click={handleTrashPressed}
-				class='absolute text-blueGray-600 top-5 right-3'
-			>
-				<TrashIcon size='24' />
+			<button on:click={handleTrashPressed} class="absolute text-blueGray-600 top-5 right-3">
+				<TrashIcon size="24" />
 			</button>
 		{/if}
-		<Input
-			label='Título'
-			bind:value={form.title}
-			type='text'
-			autofocus
-		/>
-		<TextArea
-			label='Descrição'
-			bind:value={form.description}
-			rows={5}
-		/>
-		<div class='inline-flex flex-col gap-2'>
+		<Input label="Título" bind:value={form.title} type="text" autofocus />
+		<TextArea label="Descrição" bind:value={form.description} rows={5} />
+		<div class="inline-flex flex-col gap-2">
 			{#each form.checkList as item, index (item._id)}
 				<Input
+					clearButton
 					small
 					label={!index ? 'Lista de tarefas' : ''}
 					scrollOnFocus
@@ -111,50 +94,40 @@
 					on:blur={handleTaskOnBlur}
 					on:focus={() => handleTaskOnFocus(item)}
 					on:keyup={handleTaskKeyUp}
-					placeholder={
-						index < form.checkList.length - 1
-							? 'Deixe vazio para remover'
-							: 'Nova tarefa'
-					}
+					placeholder={index < form.checkList.length - 1
+						? 'Deixe vazio para remover'
+						: 'Nova tarefa'}
 				/>
 			{/each}
 		</div>
 		<Select
-			label='Recorrência'
-			options={Object.values(RECURRENCE_TYPE).map(({key, label}) => ({value:key, label}))}
+			label="Recorrência"
+			options={Object.values(RECURRENCE_TYPE).map(({ key, label }) => ({ value: key, label }))}
 			bind:value={form.recurrence.type}
 		/>
 		{#if form.recurrence.type === RECURRENCE_TYPE.EVERY_WEEK_DAYS.key}
 			<ToggleButtons
-				label='Dias da semana'
+				label="Dias da semana"
 				multi
-				options={Info.weekdays('narrow').map((label, index) => ({label, value: index+1}))}
+				options={Info.weekdays('narrow').map((label, index) => ({ label, value: index + 1 }))}
 				bind:value={form.recurrence.weekdays}
 			/>
 		{:else if form.recurrence.type === RECURRENCE_TYPE.EVERY_MONTH_DAYS.key}
 			<ToggleButtons
-				label='Dias do mês'
+				label="Dias do mês"
 				multi
-				options={new Array(31).fill(0).map((v, index) => ({label: index + 1, value: index + 1}))}
+				options={new Array(31).fill(0).map((v, index) => ({ label: index + 1, value: index + 1 }))}
 				bind:value={form.recurrence.monthDays}
 			/>
 		{/if}
 	</Fieldset>
 
-	<div
-		class='box-border mt-auto w-full bottom-2 inline-flex justify-evenly mb-4 mt-auto'
-	>
-		<Button
-			class='!bg-blueGray-100 !text-blueGray-600'
-			on:click={handleCancelPressed}
-		>
+	<div class="box-border mt-auto w-full bottom-2 inline-flex justify-evenly mb-4 mt-auto">
+		<Button class="!bg-blueGray-100 !text-blueGray-600" on:click={handleCancelPressed}>
 			Cancelar
 		</Button>
-		<Button
-			class='!bg-teal-500'
-			on:click={handleConfirmPressed}
-		>
-			{ activity._id ? 'Salvar' : 'Adicionar' }
+		<Button class="!bg-teal-500" on:click={handleConfirmPressed}>
+			{activity._id ? 'Salvar' : 'Adicionar'}
 		</Button>
 	</div>
 </div>
--- src/lib/components/form/Input.svelte
+++ src/lib/components/form/Input.svelte
@@ -1,10 +1,14 @@
 <script>
+	import { XIcon } from 'svelte-feather-icons';
+
 	export let type = 'text';
 	export let label = '';
 	export let placeholder = '';
 	export let autofocus = false;
 	export let value = '';
 	export let scrollOnFocus = false;
+	export let clearButton = false;
+	export let inputElement;
 
 	export let small = false;
 	export let large = false;
@@ -17,55 +21,68 @@
 			event.target.scrollIntoView();
 		}
 	};
+
+	const handleClearPressed = () => {
+		inputElement.focus();
+		value = '';
+	};
 </script>
 
-<div class='w-full {$$props.class}'>
+<div class="w-full {$$props.class}">
 	{#if label}
-		<label
-			class='block uppercase text-blueGray-600 text-xs font-bold mb-2'
-			for='grid-username'
-		>
+		<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="grid-username">
 			{label}
 		</label>
 	{/if}
-	<input
-		id='grid-username'
-		on:focus={handleOnFocus}
-		on:change
-		on:focus
-		on:blur
-		on:keyup
-		class:regular={!(small || large)}
-		class:small={small}
-		class:large={large}
-		bind:value={value}
-		{placeholder}
-		{autofocus}
-	/>
+	<div class="relative">
+		<input
+			id="grid-username"
+			on:focus={handleOnFocus}
+			on:change
+			on:focus
+			on:blur
+			on:keyup
+			class:regular={!(small || large)}
+			class:small
+			class:large
+			bind:this={inputElement}
+			bind:value
+			{placeholder}
+			{autofocus}
+		/>
+		{#if clearButton && value}
+			<button
+				on:click={handleClearPressed}
+				class="absolute text-blueGray-600 bg-white right-1 top-1/2 transform -translate-y-1/2"
+			>
+				<XIcon size={small ? '20' : '24'} />
+			</button>
+		{/if}
+	</div>
 </div>
 
 <style>
-    .small {
-        @apply border-0 px-2 py-1 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full ease-linear transition-all duration-150;
-    }
+	.small {
+		@apply border-0 px-2 py-1 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full ease-linear transition-all duration-150;
+	}
 
-    .small:focus {
-        @apply outline-none ring;
-    }
+	.small:focus {
+		@apply outline-none ring;
+	}
 
-    .regular {
-        @apply border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full ease-linear transition-all duration-150;
-    }
+	.regular {
+		@apply border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full ease-linear transition-all duration-150;
+	}
 
-    .regular:focus {
-        @apply outline-none ring;
-    }
+	.regular:focus {
+		@apply outline-none ring;
+	}
 
-    .large {
-        @apply border-0 px-3 py-4 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full ease-linear transition-all duration-150;
-    }
+	.large {
+		@apply border-0 px-3 py-4 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full ease-linear transition-all duration-150;
+	}
 
-    .large:focus {
-        @apply outline-none ring;
-    }
+	.large:focus {
+		@apply outline-none ring;
+	}
 </style>
