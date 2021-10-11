7416a2683ea843fb9fb72b73e3f5e4abb7f237f1
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-10-11 15:12:25 -0300

    Updates

---
+++ _app/pages/index.svelte-a56c1e1f.js
@@ -0,0 +1,1 @@
+import"../chunks/vendor-9e23095b.js";export{R as default}from"../chunks/index-b9ebc4d0.js";import"../chunks/state-8d67a974.js";import"../chunks/FabContainer-2b8c9ead.js";import"../chunks/paths-7dc55c62.js";import"../chunks/Separator-ff86a7f8.js";
