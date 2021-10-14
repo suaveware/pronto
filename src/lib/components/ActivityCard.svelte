9f5d25809923597efcb3a0d958ef8cb58ea4a5d3
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-10-14 09:40:54 -0300

    Allow reactivation of waiting activities

--- src/lib/components/ActivityCard.svelte
+++ src/lib/components/ActivityCard.svelte
@@ -13,6 +13,7 @@
 				break;
 
 			case ACTIVITIES_STATE.DONE.key:
+			case ACTIVITIES_STATE.WAITING.key:
 				setActivityState(activity, ACTIVITIES_STATE.READY.key);
 				break;
 		}
@@ -23,24 +24,22 @@
 	class="card w-full px-4 py-3 flex bg-base-100 text-base-content select-none border-primary flex-col space-y-2"
 	on:click
 >
-	<div class="flex space-x-2 items-center">
-		{#if activity.state === ACTIVITIES_STATE.READY.key || activity.state === ACTIVITIES_STATE.DONE.key}
-			<input
-				type="checkbox"
-				checked={activity.state === ACTIVITIES_STATE.DONE.key}
-				class="checkbox border-2 checkbox-sm checkbox-primary"
-				on:click|stopPropagation={handleCheckBoxClicked}
-			/>
-		{/if}
-		<div>{activity.title}</div>
+	<div class="flex space-x-2 items-start">
+		<input
+			type="checkbox"
+			checked={activity.state !== ACTIVITIES_STATE.READY.key}
+			class="checkbox border-2 checkbox-sm checkbox-primary"
+			on:click|stopPropagation={handleCheckBoxClicked}
+		/>
+		<div class="space-y-2">
+			<div>{activity.title}</div>
+			{#if activity.state === ACTIVITIES_STATE.WAITING.key}
+				<p class="text-xs font-light">
+					{DateTime.fromISO(activity.recurrence.nextDate)
+						.toLocal()
+						.toLocaleString(DateTime.DATE_FULL)}
+				</p>
+			{/if}
+		</div>
 	</div>
-
-	{#if activity.state === ACTIVITIES_STATE.WAITING.key}
-		<p class="text-xs font-light">
-			Próxima data: {' '}
-			{DateTime.fromISO(activity.recurrence.nextDate)
-				.toLocal()
-				.toLocaleString(DateTime.DATETIME_SHORT)}
-		</p>
-	{/if}
 </div>
